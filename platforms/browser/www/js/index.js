a77c96bf8d428ae59c0458f744863aef2da78228
dzh0n <plotnikov.evgeniy@gmail.com>
2018-04-18 12:47:59 +0500

    22

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -57,5 +57,10 @@ var app = {
 
 document.addEventListener("deviceready", function(){
    alert(navigator.connection.type);
+   cordova.plugins.notification.local.schedule({
+    title: 'КОНКУРС #1021 НА ПЕРЕВОЗКУ ГРУЗОВ',
+    text: 'Перевозка извести. Стерлитамак - Салават. 3000 кг. 3 500 руб./рейс',
+    foreground: true
+});
 });
 
