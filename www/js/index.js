0981d4f87ae1df86315431d0ebb12b515085edc2
dzh0n <plotnikov.evgeniy@gmail.com>
2018-04-23 21:54:49 +0500

    s

--- www/js/index.js
+++ www/js/index.js
@@ -57,14 +57,14 @@ var app = {
 
 document.addEventListener("deviceready", function(){
     
-
+/*
      cordova.plugins.notification.local.schedule({
             id: 1021,
             title: 'КОНКУРС #1021 НА ПЕРЕВОЗКУ ГРУЗОВ',
             text: 'Перевозка извести. Стерлитамак - Салават. 3000 кг. 3 500 руб./рейс',
             data: { meetingId:"#123FG8" } 
-        });
-/*
+        });*/
+
     cordova.plugins.backgroundMode.enable();
     cordova.plugins.backgroundMode.on('activate', function () {
         setInterval(function () {
@@ -78,11 +78,11 @@ document.addEventListener("deviceready", function(){
     });
    //alert(navigator.connection.type);
 
-   cordova.plugins.notification.local.on("click", function (notification) {
+  cordova.plugins.notification.local.on("click", function (notification) {
         if (notification.id == 1021) {
             //alert(notification.data.meetingId);
             cordova.plugins.backgroundMode.disable();
         }
-    });*/
+    });
 });
 
